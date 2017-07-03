d5ae5bafc103e432dbe0decd0660ab4747f99637
cyberpunk2350 <henry@henryrice.net>
2017-07-03 17:08:59 -0400

    asdf

--- upload_profile_pic.ps1
+++ upload_profile_pic.ps1
@@ -1,9 +1,18 @@
-# input user name
-#$username = "xxxx" 
+# Maunally input user name
+#$username = "xxxx"
+
+# Pulls username from the enviromental variable
 $username = $env:UserName
+
+
 # absolute path to location of image
 #$jpgfile = "xxxx"
+
+
+# Opens file select dialog box for the JPEG
 $jpgfile = Get-OpenFile
+
+# Pushes the selcted file to the account of the curent user
 $dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 $root = $dom.GetDirectoryEntry()
 $search = [System.DirectoryServices.DirectorySearcher]
@@ -17,11 +26,12 @@ if ($result -ne $null)
  $user.setinfo() 
  Write-Host $user.displayname " updated"
 }
-else 
+else
 {
 Write-Host $struser " Does not exist"
 }
 
+# This is the function to open the file select dialog box
 Function Get-OpenFile($initialDirectory)
 {
  [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
