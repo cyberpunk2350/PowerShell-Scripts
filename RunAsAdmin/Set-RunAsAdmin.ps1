d8766a527c1e8bfa7982f9708c16724a2a391e13
cyberpunk2350 <cyberpunk2350@users.noreply.github.com>
2017-09-26 16:42:14 -0400

    quick fix to run from user terminal
    found the trick to run as other user; added a quick fix to implement change, need to still rewrite script to eliminate excess files

--- RunAsAdmin/Set-RunAsAdmin.ps1
+++ RunAsAdmin/Set-RunAsAdmin.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 
 Get-ItemProperty -path hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorUser
 
@@ -8,16 +8,20 @@ Set-ItemProperty -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\
 1 = Prompt for credentials on the secure desktop
 3 (Default) = Prompt for credentials
 
+start powershell.exe -Verb runasuser -ArgumentList ("start powershell -nonewwindow -nologo -command (.\enable_user_Run_as_admin.reg)")
 
 #>
 Param(
-[switch] $Quite
+[switch] $Quite,
+[string] $regPath = ".\" # Absolute Path to reg file
 )
 
 # Gather the relivent details, username, Admin status, and the "Run As Admin" Registary Setting
 $name = [Security.Principal.WindowsIdentity]::GetCurrent().name 
 $SecSet = (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 $AdminSet = (Get-ItemProperty -path hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorUser
+$regFile = $regPath + "enable_user_Run_as_admin.reg" #the regfile to make the change
+
 
 if ($Quite -eq $false) {Write-host "Registry Setting: " -NoNewline}
 
@@ -35,7 +39,9 @@ if (($AdminSet -eq 0) -or ($AdminSet -eq 3)) {
 		else{ .\enable_user_Run_as_admin.reg }
         Start-Sleep -Seconds 2
         if ($Quite -eq $false) {If(((Get-ItemProperty -path hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).ConsentPromptBehaviorUser) -eq 1) {Write-Host "Change Successfull"}Else{Write-Host "Change Failed" -ForegroundColor Red}}
-    } 
+    } else {
+	start powershell.exe -Verb runasuser -ArgumentList ("start powershell -nonewwindow -command ($regFile)")
+	}
 }
 
 ElseIf (($AdminSet -eq 1) -and ($Quite -eq $false)) {
@@ -45,12 +51,3 @@ ElseIf (($AdminSet -eq 1) -and ($Quite -eq $false)) {
 Else {
    if ($Quite -eq $false) {Write-host "Something went wrong" -ForegroundColor Red}
 }
-
-
-
-
-
-
-
-
-
