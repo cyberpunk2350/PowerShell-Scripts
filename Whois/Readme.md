5a9e3cbe765c5533de58d3c24cf04ddb5e8f4996
cyberpunk2350 <henry@henryrice.net>
2017-11-12 18:50:55 -0500

    inital commit
    new whois function

---
+++ Whois/Readme.md
@@ -0,0 +1,36 @@
+.SYNOPSIS
+ This is a quick hack replacement for NSLOOKUP to check mutiple DNS Servers at one time.
+
+.DESCRIPTION 
+ Quick DNS pull from your choice of DNS Servers
+ Defaults to Google and Open DNS 
+
+
+.PARAMETER Name
+    The site or address you are looking up
+    Can also use "CN","ComputerName", and "URL"
+
+.PARAMETER Server
+    The DNS server(s) you want to query 
+
+.INPUTS
+  None
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.0
+  Author:         Henry Rice
+  Creation Date:  01 Nov 2017
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  WhoIs -Name Google.com -Server 8.8.8.8
+  Using both the Name and Server Parameters
+.EXAMPLE  
+  WhoIs -Name Google.com
+  Using Just the Name Parameter
+.EXAMPLE
+  WhoIs google.com
+  Using Just the Name Parameter, however flag is not required to be included
---
+++ Whois/Whois.ps1
@@ -0,0 +1,95 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 6c21bae0-4ab4-40e2-8a49-3b8a3160f007
+
+.AUTHOR Henry Rice
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+.PRIVATEDATA 
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Quick DNS pull from your choice of DNS Servers
+ Defaults to Google and Open DNS 
+
+
+.PARAMETER Name
+    The site or address you are looking up
+    Can also use "CN","ComputerName", and "URL"
+
+.PARAMETER Server
+    The DNS server(s) you want to query 
+
+.INPUTS
+  None
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.0
+  Author:         Henry Rice
+  Creation Date:  01 Nov 2017
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  WhoIs -Name Google.com -Server 8.8.8.8
+  Using both the Name and Server Parameters
+.EXAMPLE  
+  WhoIs -Name Google.com
+  Using Just the Name Parameter
+.EXAMPLE
+  WhoIs google.com
+  Using Just the Name Parameter, however flag is not required to be included
+
+#> 
+
+Function WhoIs {
+    param (
+        [parameter(mandatory=$true)]
+        [Alias("CN","ComputerName","URL")]
+        [string[]]$Name,
+        [parameter(mandatory=$false)]
+        [string[]]$Server = ("8.8.8.8", "8.8.4.4", "208.67.222.222", "208.67.220.220")
+
+    )
+
+    foreach ($svr in $Server){
+        Write-Host (Resolve-DnsName -Name $svr -Server $svr).namehost -ForegroundColor White
+        foreach ($nm in $name){
+            try { 
+                $results = Resolve-DnsName -Name $nm -Server $svr -ErrorAction Stop 
+                $results | Format-Table 
+                } catch {
+                write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor red
+                }
+            }
+      #  Write-host " "
+    }
+}
