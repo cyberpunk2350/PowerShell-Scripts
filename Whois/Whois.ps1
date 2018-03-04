12e97b317a2563b0bd99a8b110691fe58aed3194
cyberpunk2350 <henry@henryrice.net>
2018-03-03 23:25:40 -0500

    missed one

--- Whois/Whois.ps1
+++ Whois/Whois.ps1
@@ -57,7 +57,7 @@ Function Get-DNS {
 					  v1.0:		Initial script development
 					  v1.0.1:	Changed function Name to match standard convention
 								Replaced Write-Host with Write-Output
-								Added more default Servers: quad9.net, OpenDNS, Level3.net, Symantec, DNSbyComodo.com
+								Added more default Servers: quad9.net, OpenDNS, Level3.net, Symantec, DNSbyComodo.com, ihgip.net
 	  
 	.EXAMPLE
 	  Get-DNS -Name Google.com -Server 8.8.8.8
@@ -78,7 +78,7 @@ Function Get-DNS {
         [string[]]$Name,
         [parameter(mandatory=$false)]
         [string[]]$Server = ("8.8.8.8", "8.8.4.4", "9.9.9.9" , "208.67.222.222", "208.67.220.220", "4.2.2.1", "4.2.2.2", "199.85.126.10", "199.85.127.10", "8.26.56.26", "8.20.247.20", "84.200.69.80", "84.200.70.40")
-
+##							 Google,	Google,		Quad9,		OpenDNS,		  OpenDNS,			Level3,	   Level3,	  Symantec,		   Symantec,		DNSbyComodo.com,			 ihgip.net
     )
 
     foreach ($svr in $Server){
