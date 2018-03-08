6ff12173afabf26a624ff4a2d35179c5ae76f6d3
cyberpunk2350 <cyberpunk2350@users.noreply.github.com>
2018-03-08 11:28:18 -0500

    added psscriptinfo

--- Test-Hash/Test-Hash.ps1
+++ Test-Hash/Test-Hash.ps1
@@ -1,3 +1,44 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 6e2d5345-7f63-4df7-b0e5-0253c62b0c86
+
+.AUTHOR Henry Rice
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+.PRIVATEDATA 
+
+#>
+
+<# 
+
+.DESCRIPTION 
+  Quick Hack to compare two files SHA256 hash values.  Returns True if they match and False if they don't 
+
+#> 
+
 Function Test-Hash {
 	<#
 	.SYNOPSIS 
