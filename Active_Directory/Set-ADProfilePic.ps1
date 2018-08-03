58736cb1b889e363f9223d9e3fe4c8233793fb00
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-03 14:45:50 -0400

    Rewrote to work as a function with inputs

--- Active_Directory/Set-ADProfilePic.ps1
+++ Active_Directory/Set-ADProfilePic.ps1
@@ -1,46 +1,54 @@
-# Maunally input user name
-#$username = "xxxx"
+Function Set-ADProfilePic {
 
-# Pulls username from the enviromental variable
-$username = $env:UserName
+	[CmdletBinding()]
+    Param(
+        [String] $Username = $env:UserName,  # Pulls username from the enviromental variable if one is not included
+        [String] $Picture = (Get-OpenFile),   # Opens file select dialog box for the JPEG if one is not included
+        [Switch] $AdminOverRide
+    )
 
+    if(($Username -ne $env:UserName) -and (-not $AdminOverRide)) {
+        Write-Error "Users can only set their own profile picture"
+        break
+    }elseif (($Username -ne $env:UserName) -and $AdminOverRide) {
+        Write-Warning "Attempting to change the AD Profile Picture of a Diffrent User"
+    }
+    
+    # Accesses selected user account from Active Directory
+    $dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    $root = $dom.GetDirectoryEntry()
+    $search = [System.DirectoryServices.DirectorySearcher]$root
+    $search.Filter = "(&(objectclass=user)(objectcategory=person)(samAccountName=$Username))"
+    $result = $search.FindOne()
+    if ($result -ne $null) # Pushes the selcted file to the account of the selected user
+    {
+        try{
+        $user = $result.GetDirectoryEntry() 
+        [byte[]]$jpg = Get-Content $Picture -encoding byte 
+        $user.put("thumbnailPhoto",  $jpg ) 
+        $user.setinfo() 
+        Write-Output $user.displayname "Has been updated."
+        }catch{
+            
+        }
+        
+    }
+    else
+    {
+    Write-Output $struser " Does not exist"
+    }
 
-# absolute path to location of image
-#$jpgfile = "xxxx"
-
-
-# Opens file select dialog box for the JPEG
-$jpgfile = Get-OpenFile
-
-# Pushes the selcted file to the account of the curent user
-$dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-$root = $dom.GetDirectoryEntry()
-$search = [System.DirectoryServices.DirectorySearcher]$root
-$search.Filter = "(&(objectclass=user)(objectcategory=person)(samAccountName=$username))"
-$result = $search.FindOne()
-if ($result -ne $null)
-{
- $user = $result.GetDirectoryEntry() 
- [byte[]]$jpg = Get-Content $jpgfile -encoding byte 
- $user.put("thumbnailPhoto",  $jpg ) 
- $user.setinfo() 
- Write-Host $user.displayname " updated"
+    # This is the function to open the file select dialog box
 }
-else
-{
-Write-Host $struser " Does not exist"
-}
 
-# This is the function to open the file select dialog box
-Function Get-OpenFile($initialDirectory)
-{
- [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
- Out-Null
+Function Get-OpenFile($initialDirectory) {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+    Out-Null
 
- $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
- $OpenFileDialog.initialDirectory = $initialDirectory
- $OpenFileDialog.filter = "JPEG files (*.jpg)|*.jpg"
- $OpenFileDialog.ShowDialog() | Out-Null
- $OpenFileDialog.filename
- $OpenFileDialog.ShowHelp = $true
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "JPEG files (*.jpg)|*.jpg"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+    $OpenFileDialog.ShowHelp = $true
 }
