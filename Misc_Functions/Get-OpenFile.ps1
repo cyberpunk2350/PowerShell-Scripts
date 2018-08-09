5d9aacbef3ce193018b4933f65f5117eb2e559dd
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-09 09:38:47 -0400

    Update Get-OpenFile.ps1
    Added, and a cleaned up some params

--- Misc_Functions/Get-OpenFile.ps1
+++ Misc_Functions/Get-OpenFile.ps1
@@ -1,9 +1,14 @@
-Function Get-OpenFile($initialDirectory){
+Function Get-OpenFile{
+    param(
+    [string]$initialDirectory, 
+    [string]$Title,
+    [string]$filter
+    )
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.Title = $Title
     $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = "JPEG files (*.jpg)|*.jpg"
+    $OpenFileDialog.filter = $filter   # EXAMPLE: "JPEG files (*.jpg)|*.jpg"
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filename
     $OpenFileDialog.ShowHelp = $true
