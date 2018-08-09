d1befe49cf7ee694b1174b601b029ec86e0b8b41
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-09 09:47:56 -0400

    Update Get-OpenFile.ps1
    little more clean up

--- Misc_Functions/Get-OpenFile.ps1
+++ Misc_Functions/Get-OpenFile.ps1
@@ -1,15 +1,16 @@
-Function Get-OpenFile{
+Function Get-SaveFile{ 
     param(
-    [string]$initialDirectory, 
-    [string]$Title,
-    [string]$filter
+		[string]$initialDirectory, 
+		[string]$Title = "Save As",
+		[string]$filter = "All files (*.*)| *.*"
     )
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.Title = $Title
-    $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = $filter   # EXAMPLE: "JPEG files (*.jpg)|*.jpg"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $OpenFileDialog.filename
-    $OpenFileDialog.ShowHelp = $true
+	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+	$SaveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+	$SaveFileDialog.Title = $Title
+	$SaveFileDialog.initialDirectory = $initialDirectory
+	$SaveFileDialog.filter = $filter
+	$SaveFileDialog.ShowDialog() | Out-Null
+	$SaveFileDialog.filename
+	$SaveFileDialog.ShowHelp = $true
 }
