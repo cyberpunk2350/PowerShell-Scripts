cf839c7a8a25dc53372e9291129fd63829de5b17
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-13 15:49:49 -0400

    Update Get-FileDialog.ps1
    fixed typos

--- Misc_Functions/Get-FileDialog.ps1
+++ Misc_Functions/Get-FileDialog.ps1
@@ -1,22 +1,23 @@
 Function Get-FileDialog{ 
     [CmdletBinding()]
     param(
-		[ValidateSet("Open","Save")]
+        [Parameter(Mandatory=$true, HelpMessage="Select type of File Dialog you require")]
+	[ValidateSet("Open","Save")]
         [String]$Dialog,
-        [string]$initialDirectory, 
+        [string]$InitialDirectory, 
 		[string]$Title,
-		[string]$filter = "All files (*.*)| *.*"  #"CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|Excel Worksheet (*.xls)|*.xls|All Files (*.*)|*.*"
+		[string]$Filter = "All files (*.*)| *.*"  #"CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|Excel Worksheet (*.xls)|*.xls|All Files (*.*)|*.*"
     )
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     
-    if ($Dialog -eq "Open"){$FileDialog = New-Object System.Windows.Forms.SaveFileDialog}
-    elesif ($Dialog -eq "Save"){$FileDialog = New-Object System.Windows.Forms.OpenFileDialog}
+    if ($Dialog -eq "Save"){$FileDialog = New-Object System.Windows.Forms.SaveFileDialog}
+    elseif ($Dialog -eq "Open"){$FileDialog = New-Object System.Windows.Forms.OpenFileDialog}
     else { throw "Something Went Wrong"}
     
     $FileDialog.Title = $Title
-	$FileDialog.initialDirectory = $initialDirectory
-	$FileDialog.filter = $filter
+	$FileDialog.InitialDirectory = $InitialDirectory
+	$FileDialog.Filter = $filter
 	$FileDialog.ShowDialog() | Out-Null
-	$FileDialog.filename
+	$FileDialog.FileName
 	$FileDialog.ShowHelp = $true
 }
