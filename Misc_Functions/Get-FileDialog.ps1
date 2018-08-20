a4cc41056368214f6362ce01b7a5b5246051cc6b
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-20 09:26:40 -0400

    Update Get-FileDialog.ps1
    fixed param order that broke things

--- Misc_Functions/Get-FileDialog.ps1
+++ Misc_Functions/Get-FileDialog.ps1
@@ -1,25 +1,24 @@
 Function Get-FileDialog{ 
-	[CmdletBinding()]
-    	param(
-		[Parameter(Mandatory=$true, HelpMessage="Select type of File Dialog you require")]
-		[ValidateSet("Open","Save")]
-		[String]$Dialog,
-		[String]$InitialDirectory, 
-		[String]$Title,
-		[String]$FileName,
-		[String]$Filter = "All files (*.*)| *.*"  #"CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|Excel Worksheet (*.xls)|*.xls|All Files (*.*)|*.*"
-    	)
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="Select type of File Dialog you require")]
+	[ValidateSet("Open","Save")]
+        [String]$Dialog,
+        [String]$InitialDirectory, 
+	[String]$Title,
+        [String]$Filter = "All files (*.*)| *.*",  #"CSV Files (*.csv)|*.csv|Text Files (*.txt)|*.txt|Excel Worksheet (*.xls)|*.xls|All Files (*.*)|*.*"
+	[String]$FileName
+    )
 	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     
-    	if ($Dialog -eq "Save"){$FileDialog = New-Object System.Windows.Forms.SaveFileDialog}
-    	elseif ($Dialog -eq "Open"){$FileDialog = New-Object System.Windows.Forms.OpenFileDialog}
-    	else { throw "Something Went Wrong in Dialog Box Selection"; return}
+    if ($Dialog -eq "Save"){$FileDialog = New-Object System.Windows.Forms.SaveFileDialog}
+    elseif ($Dialog -eq "Open"){$FileDialog = New-Object System.Windows.Forms.OpenFileDialog}
+    else { throw "Something Went Wrong in Dialog Box Selection"; return}
     
-	$FileDialog.Title = $Title
-	$FileDialog.InitialDirectory = $InitialDirectory
-	$FileDialog.Filter = $Filter
-    	#$FileDialog.FileName.FileName.Insert(1,$FileName)
-	$FileDialog.FileName.FileName
-	$FileDialog.ShowDialog() 
-	$FileDialog.ShowHelp = $true
+    $FileDialog.Title = $Title
+    $FileDialog.InitialDirectory = $InitialDirectory
+    $FileDialog.Filter = $Filter
+    $FileDialog.ShowDialog() | Out-Null
+    $FileDialog.FileName
+    $FileDialog.ShowHelp = $true
 }
