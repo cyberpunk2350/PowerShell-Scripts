aa5653724aea34951e884e550151f229f1e41d93
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-08-22 08:23:06 -0400

    Create Get-Certificates.ps1

---
+++ Misc_Functions/Get-Certificates.ps1
@@ -0,0 +1,21 @@
+Function Get-Certificates {
+	[CmdletBinding()]
+    param()
+	
+	Add-Type -AssemblyName System.Security
+
+	# You can do more filtering here if there are other cert requirements...
+
+	#$ValidCerts = [System.Security.Cryptography.X509Certificates.X509Certificate2[]](dir Cert:\CurrentUser\My | where { ($_.NotAfter -gt (Get-Date)) })
+	$ValidCerts = [System.Security.Cryptography.X509Certificates.X509Certificate2[]](dir Cert:\CurrentUser\My | where { $_.FriendlyName -like "Signature*" })
+
+
+	# You could check $ValidCerts, and not do this prompt if it only contains 1...
+	$Cert = [System.Security.Cryptography.X509Certificates.X509Certificate2UI]::SelectFromCollection(
+		$ValidCerts,
+		'Choose a certificate',
+		'Choose a certificate',
+		'SingleSelection'
+	) | select -First 1
+	$Cert
+}
