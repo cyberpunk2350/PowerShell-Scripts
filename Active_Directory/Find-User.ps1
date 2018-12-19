ec8de528134c8f2b19a60ae0c90acededed6a713
Henry Rice <9151488+cyberpunk2350@users.noreply.github.com>
2018-12-19 13:43:12 -0500

    Update Find-User.ps1
    added First Name, Last Name, and Title Search parameters

--- Active_Directory/Find-User.ps1
+++ Active_Directory/Find-User.ps1
@@ -2,15 +2,18 @@ Function Find-User {
 	[CmdletBinding()]
 	param (
 	[Parameter(Mandatory = $false, HelpMessage="Enter User Name to find:")]
-	[alias("UN")]
-	[String] $UserName = $null,
+		[alias("UN")]
+		[String] $UserName = $null,
 	[Switch] $Name = $false,
 	[String] $email = $null,
 	[String] $DisplayName = $null,
 	[String] $City = $null,
 	[Parameter]
-	[alias("Org")]
-	[String] $Organization = $null
+		[alias("Org")]
+		[String] $Organization = $null,
+	[String] $First = $null,
+	[String] $Last = $null,
+	[String] $Title = $null
 	)
 	$dom = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
 	$root = $dom.GetDirectoryEntry()
@@ -21,6 +24,9 @@ Function Find-User {
 	if ($UserName -ne ""){$Filter = "$Filter(samAccountName=$username)"}
 	if ($Unit -ne ""){$Filter = "$Filter(o=$Organization)"}
 	if ($Base -ne ""){$Filter = "$Filter(l=$City)"}
+	if ($First -ne ""){$Filter = "$Filter(givenname=$First)"}
+	if ($Last -ne ""){$Filter = "$Filter(sn=$Last)"}
+	if ($Title -ne ""){$Filter = "$Filter(personaltitle=$Title)"}
 	$Filter = "$Filter)"
 	$search.Filter = $Filter
 	$result = $search.FindAll()
